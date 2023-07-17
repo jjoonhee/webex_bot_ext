41535fc1aed4ccb002762decce26507efed50af1
Joonhee Cho <88135948+jjoonhee@users.noreply.github.com>
2023-07-17 21:48:48 +0900

    Update webex.py

--- webex.py
+++ webex.py
@@ -6,6 +6,7 @@ from GPT import GPT
 #Input your webex_bot token
 webex_token = ""
 #Input your company domain name so that only user in the domain can access this bot, ex: cisco.com
-bot = WebexBot(webex_token, approved_domains = ["cisco.com"])
+#bot = WebexBot(webex_token, approved_domains = ["cisco.com"])
+bot = WebexBot(webex_token)
 bot.add_command(GPT())
 bot.run()
